bab5ba3abe7934413f54c9606e65ef5d606d7639
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-10-16 09:32:28 +0200

    chore: clean files formating and remove default classes

--- SGE.Core/Entities/BaseEntity.cs
+++ SGE.Core/Entities/BaseEntity.cs
@@ -6,22 +6,22 @@ public abstract class BaseEntity
     /// Gets or sets the unique identifier for the entity.
     /// </summary>
     public int Id { get; set; }
-    
+
     /// <summary>
     /// Gets or sets the date and time when the entity was created.
     /// </summary>
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    
+
     /// <summary>
     /// Gets or sets the date and time when the entity was last updated.
     /// </summary>
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-    
+
     /// <summary>
     /// Gets or sets the identifier of the user who created the entity.
     /// </summary>
     public string CreatedBy { get; set; } = string.Empty;
-    
+
     /// <summary>
     /// Gets or sets the identifier of the user who last updated the entity.
     /// </summary>
