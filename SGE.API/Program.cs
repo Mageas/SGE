36d0a7262e0c7d9f6a520c842877326a8a8a570e
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-11-24 10:59:39 +0100

    fix: cors and docker

--- SGE.API/Program.cs
+++ SGE.API/Program.cs
@@ -36,6 +36,17 @@ builder.Services.AddScoped<ILeaveRequestService, LeaveRequestService>();
 
 builder.Services.AddControllers();
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 var app = builder.Build();
@@ -44,6 +55,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
+app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
 app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
