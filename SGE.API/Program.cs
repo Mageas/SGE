61b4ae9764132ffb15c99f970c7b5b1095922b09
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-11-24 14:29:03 +0100

    feat: add health check

--- SGE.API/Program.cs
+++ SGE.API/Program.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using SGE.API.Middleware;
 using SGE.Application.Interfaces.Repositories;
@@ -18,6 +20,10 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 // Ajouter DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+// Ajouter le service de HealthCheck
+builder.Services.AddHealthChecks()
+    .AddNpgSql(connectionString);
 
 builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfile).Assembly);
 
@@ -42,8 +48,8 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
+            .AllowAnyMethod()
+            .AllowAnyHeader();
     });
 });
 
@@ -54,9 +60,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
+app.MapHealthChecks("/api/health", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var response = report.Entries.ToDictionary(entry => entry.Key, entry => entry.Value.Status.ToString());
+        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+});
 app.UseHttpsRedirection();
 app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
+app.MapHealthChecks("/health");
 app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 app.Run();
